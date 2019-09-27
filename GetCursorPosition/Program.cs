fce141608ce62ea0da9550b4ede37b931aaeba73
Sergio Cabral <sergio.cabral@sapiensia.com>
2019-09-26 21:56:00 -0300

    Primeiro commit.

---
+++ GetCursorPosition/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace GetCursorPosition
+{
+    /// <summary>
+    /// Classe principal.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Obtem a posição do cursor na tela.
+        /// Saiba mais em: https://www.pinvoke.net/default.aspx/user32.getcursorpos
+        /// </summary>
+        /// <param name="lpPoint">Recebe os valores de posicionamento.</param>
+        /// <returns>Sucesso de retorno.</returns>
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point lpPoint);
+
+        /// <summary>
+        /// Ponto de entrada da execução do programa.
+        /// </summary>
+        private static void Main()
+        {
+            Console.WriteLine("Type any key to catch cursor position or ESC to exit.");
+            do
+            {
+                Console.WriteLine($"Keyboard X:{Console.CursorLeft} Y:{Console.CursorTop}");
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    GetCursorPos(out var mouse);
+                    Console.WriteLine($"Mouse X:{mouse.X} Y:{mouse.Y}");
+                }
+                else
+                {
+                    Console.WriteLine("Mouse cursor works only on Windows.");
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample.CSharpNETCore.GetCursorPosition
+Capturando posição do cursor do mouse e teclado.
+
+- O cursor do teclado funciona em qualquer sistema operacional quando executado como console.
+- O cursor do mouse só tem implementação para Windows (por enquanto).
